831e9d728b3711b47f7e7d6405912063836b6213
Utsavsharmame <utsavsharma@example.com>
2025-12-26 19:08:34 +0530

    first commit

---
+++ nijgnfer/productview.js
@@ -0,0 +1,35 @@
+let id = localStorage.getItem("id");
+let mainCnt = document.getElementById("mainCnt");
+
+if(id){
+    fetch(`https://fakestoreapi.com/products/${id}`)
+   .then((a)=>{
+    return a.json();
+   })
+   .then((val)=>{
+    let title = val.title;
+    let category = val.category;
+    let img = val.image;
+    let price = val.price;
+    let desc = val.description;
+    mainCnt.innerHTML = `
+    
+    
+    <div>
+
+    <img src="${img}" alt="${title}"/>
+    <h2>${title}</h2>
+    <h3>Category: ${category}</h3>
+    <h3>Price: ${price}</h3>
+    <p>${desc}</p>
+
+    </div>`
+    console.log(val);
+
+
+   })
+   .catch((err)=> {
+    console.error(err.message)
+   })
+
+}
